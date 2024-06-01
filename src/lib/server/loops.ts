71be046bf04c4ec4bfbfe2c81c65eadab89a76fa
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-06-01 22:43:24 +0200

    Delete loops hook and openai integration

--- src/lib/server/loops.ts
+++
@@ -1,6 +0,0 @@
-import { PRIVATE_LOOPS_API_KEY } from '$env/static/private';
-import { LoopsClient } from 'loops';
-
-const loops = new LoopsClient(PRIVATE_LOOPS_API_KEY);
-
-export default loops;
--- src/lib/server/openai.ts
+++
@@ -1,6 +0,0 @@
-import { PRIVATE_OPENAI_API_KEY } from "$env/static/private";
-import OpenAI from "openai";
-
-const openai = new OpenAI({apiKey: PRIVATE_OPENAI_API_KEY});
-
-export default openai;
